fa1a7840621d93f6b4c3298e7e1ed25834d7612c
Yuri Cardoso Santamarina <yurisantamarina@gmail.com>
2019-03-26 20:57:55 -0300

    New solutions

--- Solutions/Can Place Flowers.cpp
+++ Solutions/Can Place Flowers.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int cnt = 0, can = 0, a = 0;
+        for (int i = 0; i < (int)flowerbed.size(); i++) {
+            if (flowerbed[i] == 1) {
+                if (cnt == i) {
+                    a = max(0, cnt - 1);
+                } else {
+                    a = max(0, cnt - 2);
+                }
+                can += (a / 2 + (a % 2 != 0 ? 1 : 0));
+                cnt = 0;
+            } else {
+                cnt++;
+            }
+        }
+        
+        if (cnt == (int)flowerbed.size()) {
+            a = cnt;
+        } else {
+            a = max(0, cnt - 1);
+        }
+        can += (a / 2 + (a % 2 != 0 ? 1 : 0));
+        return can >= n;
+    }
+};
